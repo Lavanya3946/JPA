22e14930d150e1201a7de7820ccf8077e3bad6af
Lavanya121003 <lavanyaanand1210@gmail.com>
2025-01-07 22:34:49 +0530

    jdbc fetching by fields

---
+++ waterBottel/src/main/java/com/xworkz/waterBottel/runner/FetchAllData.java
@@ -0,0 +1,41 @@
+package com.xworkz.waterBottel.runner;
+
+import java.sql.*;
+
+public class FetchAllData {
+    public static void main(String[] args) {
+        Connection connection=null;
+        Statement statement=null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url="jdbc:mysql://localhost:3306/product";
+            String userName="root";
+            String password="root";
+            connection= DriverManager.getConnection(url,userName,password);
+            statement=connection.createStatement();
+            String query="select * from product_info";
+            ResultSet resultSet=statement.executeQuery(query);
+
+            while(resultSet.next()) {
+                int productID = resultSet.getInt(1);
+                String productName = resultSet.getString(2);
+                Double productPrice = resultSet.getDouble(3);
+
+                System.out.println("ProductId: "+productID+"  Product Name: "+productName+"  Product prize: "+productPrice);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }finally{
+            try {
+                if (connection != null){
+                    connection.close();
+            }else if(statement!=null){
+                connection.close();
+            }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
---
+++ waterBottel/src/main/java/com/xworkz/waterBottel/runner/FetchByTwoFields.java
@@ -0,0 +1,42 @@
+package com.xworkz.waterBottel.runner;
+
+import java.sql.*;
+
+public class FetchByTwoFields {
+    public static void main(String[] args) {
+        Connection connection=null;
+        Statement statement=null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url="jdbc:mysql://localhost:3306/product";
+            String userName="root";
+            String password="root";
+            connection= DriverManager.getConnection(url,userName,password);
+            statement=connection.createStatement();
+            String query="select * from product_info where p_id =3 AND p_name='plates'";
+            ResultSet resultSet=statement.executeQuery(query);
+
+            while(resultSet.next()) {
+                int productID = resultSet.getInt(1);
+                String productName = resultSet.getString(2);
+                Double productPrice = resultSet.getDouble(3);
+
+                System.out.println("ProductId: "+productID+"  Product Name: "+productName+"  Product prize: "+productPrice);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }finally{
+            try {
+                if (statement != null){
+                    statement.close();
+                }else if(connection!=null){
+                    connection.close();
+                }
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    }
+
